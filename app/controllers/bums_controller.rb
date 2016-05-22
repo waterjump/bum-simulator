31e78e631c1133dbf128c8549828b1f1a159c121
Sean Barrett <sbarrett@weblinc.com>
2016-05-22 15:26:14 -0400

    initial

---
+++ app/controllers/bums_controller.rb
@@ -0,0 +1,63 @@
+class BumsController < ApplicationController
+
+  before_filter :bum_session, except: [:new, :show, :create]
+
+  def new
+    reset_session
+  end
+
+  def show
+    Rails.logger.info "69BOT - show"
+    Rails.logger.info "69BOT - session: #{session.inspect}"
+    if current_user.present?
+      Rails.logger.info "69BOT - user present"
+      @bum = Bum.find_or_initialize(current_user.id)
+    elsif session[:bum_id].present?
+      Rails.logger.info "69BOT - session present"
+      @bum = Bum.find(session[:bum_id])
+    else
+      redirect_to new_bum_path
+    end
+
+  end
+
+  def create
+    Rails.logger.info "69BOT - create"
+    @bum = Bum.create!
+    Rails.logger.info "69BOT - @bum.inspect: #{@bum.inspect}"
+    session[:bum_id] = @bum.id
+    redirect_to bum_path
+  end
+
+  # Game actions
+
+  def panhandle
+    @bum.panhandle
+    render 'show'
+  end
+
+  def sleep
+    @bum.sleep(params['hours'].to_i)
+    render 'show'
+  end
+
+  def drink_beer
+  end
+
+  def rummage
+  end
+
+  def eat
+  end
+
+  protected
+
+  def bum_session
+    if session[:bum_id].present?
+      @bum = Bum.find(session[:bum_id])
+    else
+      @bum = Bum.create!
+      session[:bum_id] = @bum.id
+    end
+  end
+end
---
+++ app/models/bum.rb
@@ -0,0 +1,103 @@
+class Bum
+  include Mongoid::Document
+
+  field :user_id, type: String
+  field :money, type: Integer, default: 0
+  field :energy, type: Integer, default: 16
+  field :calories, type: Integer, default: 1000
+  field :appeal, type: Integer, default: 10
+  field :diary, type: Hash, default: {}
+  field :time, type: DateTime, default: DateTime.new(1983,4,3,8)
+  field :life, type: Integer, default: 1000
+
+  def self.find_or_initialize(user_id)
+    where(user_id: user_id).first || create!
+  end
+
+  def panhandle
+    gonna_die
+    earnings = (10 * appeal * luck * (life/1000.0)) # cents
+    self.money += earnings if earnings > 0
+    write_in_diary("panhandled for one hour and made #{format_money(earnings)}.")
+    pass_one_hour
+    save
+  end
+
+  def sleep(hours)
+    self.energy += hours * 2
+    self.energy = 16 if self.energy > 16
+    get_robbed_in_sleep
+    s = hours > 1 ? 's' : ''
+    write_in_diary("slept for #{hours} hour#{s}.")
+    hours.times { pass_one_hour(100, true) }
+    save
+  end
+
+  def drink_beer
+    return unless check_price(250)
+    self.money -= 250
+    self.life += 200
+    write_in_diary("drank a beer.")
+    save
+  end
+
+  def rummage
+    # TODO
+  end
+
+  def eat(food)
+    # TODO
+  end
+
+  private
+
+  def health_check
+
+  end
+
+  def gonna_die
+    return unless self.life <= 300
+    write_in_diary("feel like death.")
+  end
+
+  def check_price(price)
+    result = price <= self.money
+    write_in_diary('don\'t have enough money for that.')
+    result
+  end
+
+  def format_money(amount)
+    "$#{(amount/100.00).round(2)}"
+  end
+
+  def get_robbed_in_sleep
+    return if luck >= 0.74 # One in 14 chance
+    amount = self.money * 0.4 * luck
+    self.money -= amount
+    write_in_diary("got robbed while asleep for #{format_money(amount)}! Fuck!")
+  end
+
+  def pass_one_hour(calories = 100, sleeping = false)
+    self.time += 1.hour
+    self.energy -= 1 unless sleeping
+    self.calories -= calories
+    if self.energy <= 0
+      self.life -= 100
+      self.energy = 0
+    end
+    if self.calories <= 0
+      self.life -= calories * 1
+      self.calories = 0
+    end
+  end
+
+  def write_in_diary(entry)
+    puts "#{time.strftime('%b %-d, %Y')}: You #{entry}"
+    diary.merge!("#{time.to_s}" => entry)
+  end
+
+  def luck
+    # random number between 0.70 and 1.30
+    (Random.new.rand(60) + 70.0) / 100.0
+  end
+end
